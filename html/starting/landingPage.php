4597498a868b13bf16e710091bfc9f820464778c
Ari Walls <79683340+AriOlguin@users.noreply.github.com>
2022-12-07 18:18:03 -0600

    corrrecion de colores

--- html/starting/landingPage.php
+++ html/starting/landingPage.php
@@ -6,6 +6,7 @@
     <title>Cuidado con el duende</title>
     <link rel="stylesheet" href="../../css/bootstrap.css">
     <link rel="stylesheet" href="../../css/Nuevo.css">
+    <link rel="stylesheet" href="../../css/style.css">
 </head>
 <body>
     <header>
@@ -16,8 +17,8 @@
             <div class="menu">
                 <nav id="nav">
                     <ul>
-                        <li><button onclick='location.href="registro.html"' type="button" class="btn btn-primary" data-toggle="button" aria-pressed="false" autocomplete="off">Registrarse</button></li>
-                        <li><button type="button" class="btn btn-primary" data-toggle="button" aria-pressed="false" autocomplete="off">Ayuda</button></li>
+                        <li><button onclick='location.href="registro.html"' type="button" class="btn btn-outline-success" data-toggle="button" aria-pressed="false" autocomplete="off">Registrarse</button></li>
+                        <li><button type="button" class="btn btn-outline-success" data-toggle="button" aria-pressed="false" autocomplete="off">Ayuda</button></li>
                     </ul>
                 </nav>
             </div>
@@ -79,18 +80,125 @@
                             <i class="fas fa-exclamation-circle"></i>
                             <small>Error</small>
                         </div>
-                        <button>Iniciar sesión</button>
+                        <button class = "btn btn-outline-success">Iniciar sesión</button>
                     </form>
                 </div>
             </div>
         </div>   
     </main>
     <!--Footer-->
-    <div class="container-footer">
-        <footer>
-            <h4>Todos los derechos Reservados 2022</h4>
-        </footer>
+    <footer>
+  <div class = "container" id="footer-gray">
+    <div class="footer-custom">
+      <div class="footer-lists">
+        <div class="footer-list-wrap">
+          <h6 class="ftr-hdr">Linea de soporte</h6>
+          <ul class="ftr-links-sub">
+            <li>800-8186-6666</li>
+          </ul>
+        </div>
+        <!--/.footer-list-wrap-->
+        <div class="footer-list-wrap">
+          <h6 class="ftr-hdr">Servicio a Cliente</h6>
+          <ul class="ftr-links-sub">
+            <li><a href="">Contactanos</a></li>
+            <li><a href="">Ordena</a></li>
+            <li><a href="">Metodos de pago</a></li>
+            <li><a href="">FAQs</a></li>
+          </ul>
+        </div>
+        <div class="footer-list-wrap">
+          <h6 class="ftr-hdr">Sobre Nosotros</h6>
+          <ul class="ftr-links-sub">
+            <li><a href="">Nuestra Empresa</a></li>
+            <li><a href="">Trabaja con nosotros</a></li>
+            
+            <li><a href="/catalog" rel="nofollow">Nuestros productos</a></li>
+          </ul>
+        </div>
+        <!--/.footer-list-wrap-->
+        <div class="footer-list-wrap">
+          <h6 class="ftr-hdr"></h6>
+          <ul class="ftr-links-sub">
+              <li class="ftr-Login"><a href="../usuarios/c-profile.php"></a></li>
+              <li><a href="../comprador/c-misPedidos.php"></a></li>
+              <li><a href="../comprador/c-carrito.php"></a></li>
+              <li><a href="../../php/includes/usuarios/close_session_inc.php"></a></li>       
+          </ul>
+        </div>
+        <!--/.footer-list-wrap-->
+      </div>
+      <!--/.footer-lists-->
+
+      <!--/.footer-email-->
+      <div class="footer-social">
+        <h6 class="ftr-hdr">Siguenos</h6>
+        <ul>
+          <li>
+            <a href="" title="Facebook">
+                <span title="Facebook"> <img src='../../resources/logos/facebook.png' class='imgRedonda'/></span>
+            </a>
+          </li>
+          <li>
+            <a href="" title="Instagram">
+                <span title="Instragam"> <img src='../../resources/logos/instagram.png' class='imgRedonda'/></span>
+            </a>
+          </li>
+          <li>
+            <a href="" title="WhatsApp">
+                <span> <img src='../../resources/logos/whatsapp.png' class='imgRedonda'/></span>
+            </a>
+          </li>
+          <li>  
+            <a href=""  title="Youtube">
+                <span> <img src='../../resources/logos/youtube.jpg' class='imgRedonda'/></span>
+            </a>
+          </li>
+        </ul>
+      </div>
+      <!--/.footer-social-->
+      <div class="footer-legal">
+        <p>
+            &copy; CUIDADO CON EL DUENDE, Todos los derechos reservados 2022 | 
+            <a href="">Politica de privacidad</a> | 
+            <a href="">Términos de uso</a> 
+
+        </p>
+        <p>CUIDADO CON EL DUENDE es una marca registrada de CUIDADO CON EL DUENDE SA DE CV</p>
+        <p>Este sitio web contiene material con derechos de autor cuyo uso no ha sido autorizado por los propietarios de los derechos de autor. Creemos que este uso educativo sin fines de lucro en la Web constituye un uso justo del material con derechos de autor (según lo estipulado en la sección 107 de la Ley de derechos de autor de EE.UU.).</p>
+      </div>
+      <!--/.footer-legal-->
+      <div class="footer-payment">
+        <ul>
+          <li>
+          <span title="Pay Pal"> <img src='../../resources/logos/paypal-logo.jpg' class='imgCuadrada'/></span>
+          </li>
+          <li>
+          <span title="Master Card"> <img src='../../resources/logos/Mastercard-Simbolo.jpg' class='imgCuadrada'/></span>
+          </li>
+          <li>
+            <span title="Visa"> <img src='../../resources/logos/Visa-Emblem.jpg' class='imgCuadrada'/></span>
+          </li>
+        </ul>
+      </div>
+      <!--/.footer-payment-->
     </div>
+    <!--/.footer-custom-->
+  </div>
+  <!--/.footer-gray-->
+</footer>
+<div class = "container-flex" id = "color2">
+    <h12>.</h12>
+  </div>
+  <div class = "container-flex" id = "color1">
+    <h12>.</h12>
+  </div>
+  <div class = "container-flex" id = "color3">
+    <h12>.</h12>
+  </div>
+  <div class = "container-flex" id = "color4">
+    <h12>.</h12>
+  </div>
     <script src="../../js/lib/bootstrap.bundle.js"></script>
     <script src="../../js/lib/jquery-3.6.1.js"></script>
     <script src="../../js/utilities/validaciones.js"></script>
